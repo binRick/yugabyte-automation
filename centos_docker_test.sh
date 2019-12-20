7e7fb55541f070f475189a79baabb9cf4d24112e
root <root@web1.vpnservice.company>
2019-12-20 08:45:36 -0500

    initial yuga script ideas

---
+++ centos_docker_test.sh
@@ -0,0 +1,3 @@
+docker run  -it centos:latest curl https://google.com
+docker run  -it centos:latest ping -c 1 -w 1 google.com
+docker run  -it centos:latest ping -c 1 -w 1 4.2.2.1
---
+++ redis_server.py
@@ -0,0 +1,1 @@
+docker exec -it yb-master-n1 /home/yugabyte/bin/yb-admin --master_addresses yb-master-n1:7100 setup_redis_table
