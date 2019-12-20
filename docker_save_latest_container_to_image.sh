28a19b767e4bd9516a6a2d3ada2569b4bcda6d53
rick <rick@ricks-MacBook-Air.local>
2019-12-20 14:14:51 -0500

    grab newest container for img

--- docker_save_latest_container_to_image.sh
+++ docker_save_latest_container_to_image.sh
@@ -1,1 +1,1 @@
-time docker commit $(docker ps -a|grep centos:latest|head -n1|cut -d' ' -f1) centos:latest
+time docker commit $(docker ps -a|grep centos:latest|tail -n1|cut -d' ' -f1) centos:latest
