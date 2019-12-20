5892218da7b758a4f28c6afd80ed10c09f4bebd1
root <admin@vpntech.net>
2019-12-20 09:56:21 -0500

    more ideas

--- docker_load_remote_image.sh
+++ docker_load_remote_image.sh
@@ -1,4 +1,20 @@
+set -e
+
 
 REMOTE_HOST=xxxxxxxx
 
-time sh -c "docker ps -a |cut -d' ' -f1|grep -v CONTA|xargs docker rm; docker image rm centos:latest;  time ssh -q root@$REMOTE_HOST 'unlink /root/centos-latest.tar; docker save centos:latest -o /root/centos-latest.tar' && rsync root@${REMOTE_HOST}:centos-latest.tar ~/ && time docker load < ~/centos-latest.tar"
+ssh root@$REMOTE_HOST docker images|grep '^centos '|grep ' latest '
+
+docker ps -a |cut -d' ' -f1|grep -v CONTA|xargs docker rm
+
+docker image rm centos:latest -f
+
+
+time ssh -q root@$REMOTE_HOST \
+	'unlink /root/centos-latest.tar; \
+  	docker save centos:latest -o /root/centos-latest.tar'
+
+
+time rsync root@${REMOTE_HOST}:centos-latest.tar ~/
+
+time docker load < ~/centos-latest.tar
